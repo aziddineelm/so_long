8925b15988ccafe618ddacb5020e49cacf00da43
aziddineelm <azedine160@gmail.com>
2025-02-10 12:26:23 +0100

    .

--- so_long.c
+++ so_long.c
@@ -1,28 +1,30 @@
 #include "so_long.h"
-#include <mlx.h>
-#include <stdlib.h>
 
 int	close_window(int keycode, t_data *vars)
 {
 	if (keycode == 65307)
 	{
-		mlx_destroy_window(vars->mlx, vars->win);
+		mlx_destroy_window(vars->mlx, vars->window);
 		exit(0);
 	}
 	return (0);
 }
 
-int	main(void)
+void	load_map(void)
 {
-	t_data	*data;
+	t_data	vars;
 
-	data = (t_data *)malloc(sizeof(t_data));
-	data->mlx = mlx_init();
-	data->win = mlx_new_window(data->mlx, 1920, 1080, "Hello world!");
-	data->img = mlx_xpm_file_to_image(data->mlx, "./alien.xpm", &data->width,
-			&data->hight);
-	mlx_put_image_to_window(data->mlx, data->win, data->img, 68, 68);
-	mlx_hook(data->win, 2, 1L << 0, close_window, &data->mlx);
-	mlx_loop(data->mlx);
+	vars.mlx = mlx_init();
+	vars.window = mlx_new_window(vars.mlx, 740, 480, "so_long");
+	mlx_key_hook(vars.window, close_window, &vars);
+	mlx_loop(vars.mlx);
+}
+
+int	main(int ac, char **map)
+{
+	if (ac == 2)
+	{
+		load_map();
+	}
 	return (0);
 }
--- so_long.h
+++ so_long.h
@@ -6,20 +6,20 @@
 /*   By: ael-mans <ael-mans@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 18:52:31 by ael-mans          #+#    #+#             */
-/*   Updated: 2025/02/07 19:21:06 by ael-mans         ###   ########.fr       */
+/*   Updated: 2025/02/09 11:04:41 by ael-mans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# include <mlx.h>
+# include <stdlib.h>
+
 typedef struct s_data
 {
 	void	*mlx;
-	void	*win;
-	void	*img;
-	int		width;
-	int		hight;
+	void	*window;
 }			t_data;
 
 #endif
